4eac5f098fd7ad5a20f640230a3f6be1d873d828
ilathem <isaiahlathem@gmail.com>
2023-11-21 17:31:21 -0600

    Implement round of play

--- index.js
+++ index.js
@@ -6,3 +6,33 @@ function getComputerChoice() {
     return randomChoice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    const normalizedPlayerSelection = playerSelection.toLowerCase();
+    const normalizedComputerSelection = computerSelection.toLowerCase();
+
+    const winningScenarios = [
+        ['paper', 'rock'],
+        ['rock', 'scissors'],
+        ['scissors', 'paper']
+    ]
+
+    const currentScenario = [
+        normalizedPlayerSelection, 
+        normalizedComputerSelection
+    ];
+
+    let wonRound = false;
+    for (const winningScenario of winningScenarios) {
+        if (winningScenario[0] === currentScenario[0] &&
+            winningScenario[1] === currentScenario[1]) {
+            wonRound = true;
+        }
+    }
+
+    if (wonRound)
+        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    else 
+        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+
+}
+

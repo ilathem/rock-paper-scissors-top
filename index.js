2f00bc6d2658097bdd51d4de660a83b56ca2d030
ilathem <isaiahlathem@gmail.com>
2023-11-21 17:57:06 -0600

    Add input validation and same selection guards

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ function playRound(playerSelection, computerSelection) {
         normalizedComputerSelection
     ];
 
+    if (normalizedPlayerSelection === normalizedComputerSelection) 
+        return `Try Again! You both chose ${normalizedComputerSelection}`
+    
+
+    if (!['rock', 'paper', 'scissors'].includes(normalizedPlayerSelection)) 
+        return `Try Again! ${playerSelection} is not one of the options!`
+
     let wonRound = false;
     for (const winningScenario of winningScenarios) {
         if (winningScenario[0] === currentScenario[0] &&
@@ -36,3 +43,32 @@ function playRound(playerSelection, computerSelection) {
 
 }
 
+function game() {
+    const score = {
+        player: 0,
+        computer: 0,
+    }
+    let roundCount = 0;
+    let keepGoing = true;
+    while (keepGoing) {
+        const playerSelection = prompt("Please enter rock, paper, or scissors")
+        const result = playRound(playerSelection, getComputerChoice());
+        console.log(result);
+        if (result.includes('Win'))
+            score.player++;
+        else if (result.includes('Lose'))
+            score.computer++;
+        if (!result.includes('Try Again')) {
+            roundCount++;
+        }
+        if (roundCount === 5) keepGoing = false;
+    }
+    if (score.player > score.computer) {
+        console.log("Congratulations! You are the Rock, Paper, Scissors Champion!")
+    } else {
+        console.log("Sorry! You are not the Rock, Paper, Scissors Champion.")
+    }
+    console.log(`Final score (You - CPU): ${score.player} - ${score.computer}`)
+}
+
+game();
